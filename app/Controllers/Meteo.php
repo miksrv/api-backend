2daeec1de6103560032f9323988c482b17ae5ea9
Mike <miksoft.tm@gmail.com>
2021-02-02 19:58:38 +0500

    refractor: Add new meteo controller

---
+++ app/Controllers/Meteo.php
@@ -0,0 +1,30 @@
+<?php namespace App\Controllers;
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+
+
+/**
+ * Class Astro
+ * @package App\Controllers
+ */
+class Meteo extends BaseController
+{
+    protected $_webcam_url = 'http://astro.myftp.org:8002/jpg/1/image.jpg';
+
+    function set($action)
+    {
+        switch ($action)
+        {
+            default : throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+    }
+
+    function get($action)
+    {
+        switch ($action)
+        {
+            default : throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
+        }
+    }
+}
