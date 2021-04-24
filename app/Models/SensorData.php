9bcd906416c087df2732e2b04253998a316a13b6
Mike <miksoft.tm@gmail.com>
2021-04-24 09:41:34 +0500

    refractor: Sesnsor model optimize

--- app/Models/SensorData.php
+++ app/Models/SensorData.php
@@ -19,44 +19,13 @@ class SensorData extends Model
      * #TODO Optimize
      * @return mixed
      */
-    public function get_period($source = 'meteo', $period = 'today', $date = null, $daterange = null)
+    public function get_period($source = 'meteo', $daterange = null)
     {
         $this->table = getenv('database.table.' . $source . '_data');
 
-        /**
-         * date - выбирается дата за день, от начала дня до его конца, или до последней записи (если текущией день)
-         * date_start - начало периода, день с начала 00:00
-         * date_end - концев периода, окончание дня до 23:59:59
-         */
-
-        if ($date) {
-            return $this->db->table($this->table)
-                ->where("DATE_FORMAT(item_timestamp, '%Y-%m-%d') = '{$date}'")
-                ->orderBy('item_timestamp', 'DESC')
-                ->get()
-                ->getResult();
-        }
-
-        // Feature
-        if ($daterange) {
-            return $this->db->table($this->table)
-                ->where("DATE_FORMAT(item_timestamp, '%Y-%m-%d') BETWEEN '{$daterange->start}' AND '{$daterange->end}'")
-                ->orderBy('item_timestamp', 'DESC')
-                ->get()
-                ->getResult();
-
-        }
-
-        // Deprecated
-        switch ($period) {
-            case 'today'     : $period = 'DATE_SUB(NOW(), INTERVAL 1 DAY)'; break;
-            case 'yesterday' : $period = 'CURDATE() - INTERVAL 1 DAY'; break;
-            case 'week'      : $period = 'DATE_SUB(NOW(), INTERVAL 7 DAY)'; break;
-            case 'month'     : $period = 'DATE_SUB(NOW(), INTERVAL 30 DAY)'; break;
-        }
-
         return $this->db->table($this->table)
-            ->where('`item_timestamp` >= ' . $period)
+            ->where("item_timestamp BETWEEN '{$daterange->start}' AND '{$daterange->end}'")
+            // ->where("DATE_FORMAT(item_timestamp, '%Y-%m-%d') BETWEEN '{$daterange->start}' AND '{$daterange->end}'")
             ->orderBy('item_timestamp', 'DESC')
             ->get()
             ->getResult();
