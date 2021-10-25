e185e29f2bc3d613ba4d11e99f9fb8d440536c62
Mik <miksoft.tm@gmail.com>
2021-10-25 21:12:51 +0500

    fix: Temp add class

--- app/Libraries/OpenWeather.php
+++ app/Libraries/OpenWeather.php
@@ -2,16 +2,64 @@
 
 namespace App\Libraries;
 
-/**
- * Class OpenWeather
- */
+use App\Models\Weather as modelWeather;
 
 class OpenWeather {
-    // OpenWeather API URL
-    const API_URL = 'http://api.openweathermap.org/data/2.5/forecast';
+    const DEVICE_ID   = 'openweather';
+    const API_VERSION = 2.5;
+    const API_URL     = 'http://api.openweathermap.org/data/' . self::API_VERSION . '/';
 
-    // OpenWeatherMap result cache time in sec
-    const CACHE_TIME = 600;
+    private $_modelWeather;
+    private $_serviceCURL;
+
+    function __construct()
+    {
+        $this->_serviceCURL  = \Config\Services::curlrequest();
+        $this->_modelWeather = new modelWeather();
+    }
+
+    function parse()
+    {
+        $endpoint = 'weather?lat=' . getenv('app.latitude') . '&lon=' . getenv('app.longitude');
+        $response = $this->_endpoint($endpoint);
+
+        $data = (object) [
+            't' => round($response->main->temp, 1),
+            'h' => $response->main->humidity,
+            'p' => round($response->main->pressure / 1.333, 1),
+            'ws' => $response->wind->speed,
+            'wd' => $response->wind->deg,
+            'wg' => $response->wind->gust,
+            'c'  => $response->clouds->all,
+            'r'  => $response->rain->{'1h'} ?? 0,
+            's'  => $response->snow->{'1h'} ?? 0,
+        ];
+
+        $this->_modelWeather->add(self::DEVICE_ID, json_encode($data));
+
+        $meteo = (object) [
+            'p' => round($response->main->pressure / 1.333, 1),
+            't1' => round($response->main->temp, 1),
+            't2' => round($response->main->temp, 1),
+            'h' => $response->main->humidity,
+            'lux' => 0,
+            'uv' => 0,
+            'wd' => $response->wind->deg,
+            'ws' => $response->wind->speed
+        ];
+
+        $this->_modelWeather->add_meteo(json_encode($meteo));
+    }
+
+    private function _endpoint($name)
+    {
+        $response = $this->_serviceCURL->get(self::API_URL . $name . '&appid=' . getenv('app.openweather.key') . '&units=metric&lang=ru');
+
+        return json_decode($response->getBody());
+    }
+
+
+
 
     /**
      * Weather forecast from OpenWeatherMap service
@@ -22,7 +70,7 @@ class OpenWeather {
         if ( ! $foreacst = cache('forecast'))
         {
             $client   = \Config\Services::curlrequest();
-            $api_url  = self::API_URL . '?id=' . getenv('app.openweather.city') . '&appid=' . getenv('app.openweather.key') . '&units=metric&lang=ru';
+            $api_url  = 'http://api.openweathermap.org/data/2.5/forecast' . '?id=' . getenv('app.openweather.city') . '&appid=' . getenv('app.openweather.key') . '&units=metric&lang=ru';
             $response = $client->get($api_url);
 
             if ($response->getStatusCode() !== 200)
@@ -38,7 +86,7 @@ class OpenWeather {
 
             $foreacst = $response->getBody();
 
-            cache()->save('foreacst', $foreacst, self::CACHE_TIME);
+            cache()->save('foreacst', $foreacst, 600);
         }
 
         return (object) [
